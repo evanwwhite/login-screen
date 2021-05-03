aaaba17cf2747fab51e86d2c6f65c61bc8d65df2
Evan White <69662443+evanwwhite@users.noreply.github.com>
2021-05-03 14:00:33 -0500

    Update LoginScreen.java

--- LoginScreen.java
+++ LoginScreen.java
@@ -9,17 +9,17 @@ import java.util.ArrayList;
 
 public class LoginScreen extends JFrame implements ActionListener{
 	static private ArrayList <BankerClass> customers;
-    private String name;
+    //private String name;
     private JPanel mainPanel;
     private JLabel passLabel, userLabel;
     private JTextField userTxt;
     private JPasswordField passwordField;
     private JButton login, exit;
-
+    
   
 	public static void main(String[] args) throws IOException{
 		
-		//Scanner input = new Scanner(new File("login.txt"));
+		Scanner input = new Scanner(new File("login.txt"));
 		customers = new ArrayList <BankerClass> ();
 		JFrame frame = new LoginScreen();
 
@@ -36,62 +36,80 @@ public class LoginScreen extends JFrame implements ActionListener{
 
 		frame.addWindowListener(w);
 		
-	/*input.nextLine();
-	input.nextLine();
-
-	while(input.hasNext())
-	{
-		name = input.nextLine();
-	}*/
-    }
+		
 	
+	input.nextLine();
+	input.nextLine();
+	
+	 while(input.hasNext())
+	 {
+	 	String name = input.nextLine();
+	 	String pass = input.nextLine();
+	 	double amount = Double.parseDouble(input.nextLine());
+	 }
+    }
+
 
 
 	LoginScreen(){
+		
         mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(2,3));
-        mainPanel.setPreferredSize(new Dimension(500,600));
-		mainPanel.setBorder(BorderFactory.createMatteBorder(25,25,25,25, Color.gray));
+        mainPanel.setLayout(new GridLayout(3,2));
+        mainPanel.setPreferredSize(new Dimension(700,400));
+		mainPanel.setBorder(BorderFactory.createMatteBorder(20,20,20,20, Color.gray));
 		
-        passLabel = new JLabel();
-		passLabel.setBorder(BorderFactory.createMatteBorder(25,25,25,25, Color.gray));
+        setContentPane(mainPanel);
+        
+        
 		
-		userLabel = new JLabel();
-		userlabel.setBorder(BorderFactory.createMatteBorder(25,25,25,25, Color.gray));
-
-		userTxt = new JTextField();
-		userTxt.setBorder(BorderFactory.createMatteBorder(25,25,25,25, Color.gray));
-
-		passwordField = new JPasswordField();
-		passwordField.setBorder(BorderFactory.createMatteBorder(25,25,25,25, Color.gray));
-
-		login = new JButton();
-		login.setBorder(BorderFactory.createMatteBorder(25,25,25,25, Color.gray));
 		
-        exit = new JButton();
-		exit.setBorder(BorderFactory.createMatteBorder(25,25,25,25, Color.gray));
+		mainPanel.add(userLabel = new JLabel("Username"));
+		
+		mainPanel.add(userTxt = new JTextField());
+		
+		mainPanel.add(passLabel = new JLabel("Password"));
+		
 
+		
+		
 
+		mainPanel.add(passwordField = new JPasswordField());
+		
 
+		mainPanel.add(login = new JButton("Login"));
+		
+		
+        mainPanel.add(exit = new JButton("Exit"));
+		exit.addActionListener(this);
 
-
-
+		
     }
 
 	 public void actionPerformed(ActionEvent w) {
-	
+		 int counter = 0;
 	  if(w.getSource() == exit) {
           System.exit(0);
        }
 	  
-	  /*if(w.getSource() == login) { 
+	  if(w.getSource() == login) { 
+		  
+		  boolean found = false;
+		  
 		  for(int i = 0; i < customers.size(); i++) {
-			  if(userField.getText().equals(customers.get(i).getName()) &&
+			  if(userTxt.getText().equals(customers.get(i).getName()) &&
 					  passwordField.getText().equals(customers.get(i).getPassword()));
+			  
+			  userTxt.setText(String.format("s%.2f", customers.get(i).getBalance()));
+					  found = true;
 					  
+			if(found == false) {
+						  counter++;
+						  userTxt.setText("");
+						  passwordField.setText("");
+					  }
 		  }
 	
-	  }*/
+	  }
 	  
 	  
 	  
